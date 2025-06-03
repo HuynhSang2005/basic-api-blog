47c9e2415d18915bbd26db1b52e8205c030be7d1
HuynhSang2005 <huynhsang060305@gmail.com>
2025-06-03 18:36:01 +0700

    feat: enhance user profile update schema with avatar URL validation and transformation

--- src/main.ts
+++ src/main.ts
@@ -35,6 +35,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document, {
+    jsonDocumentUrl: '/api-docs-json',
     swaggerOptions: {
       persistAuthorization: true, 
       tagsSorter: 'alpha',
